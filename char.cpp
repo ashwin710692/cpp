a465269c878e248f3c4d57d1fcca283789bc75dd
ashwin710692 <58033880+ashwin710692@users.noreply.github.com>
2022-01-08 12:24:30 +0530

    myc

---
+++ char.cpp
@@ -0,0 +1,30 @@
+ #include<iostream>    // product program 
+ using namespace std;
+
+int main() {
+
+  
+  float num1 = 3.4 ;
+  float num2= 5.5;
+  
+  float sum = 3.4*5.5;
+  cout<<"prouduct:"<<sum;
+}
+
+
+
+
+ #include<iostream>
+ using namespace std;
+ 
+ int main() 
+ { 
+ 
+                                                                          //for sixe of characters
+ cout<<"size of int is : "<<sizeof(int)<<"byte" << endl;
+ cout<<"size of char is :"<<sizeof(char)<<"byte"<<endl;
+ cout<<"size of float is :"<<sizeof(float)<<"byte"<<endl;
+ cout<<"size of double is :"<<sizeof(double)<<"byte"<<endl;
+ 	
+ 	
+ }
